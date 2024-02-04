9ac77681d6acff30b4e7a03a75fac5d61e702db6
Everest1508 <riteshmahale15@gmail.com>
2024-02-04 23:13:07 +0530

    auth

--- room/urls.py
+++ room/urls.py
@@ -5,9 +5,9 @@ from .views import *
 urlpatterns = [
     path('patients/',PatientListView.as_view() ),
     path('room/',RoomWithBedAPIView.as_view() ),
-    path('discharge/<int:patient_id>',DischargePatientAPIView.as_view()),
+    path('discharge/<int:patient_id>/',DischargePatientAPIView.as_view()),
     path('change-room-clean-status/<int:room_number>/', RoomCleanStatusAPIView.as_view(), name='change-room-clean-status'),
-    path('change-medication-status/<int:medication_id>',MedicationStatusAPIView.as_view(),name="change-medication-status"),
+    path('change-medication-status/<int:medication_id>/',MedicationStatusAPIView.as_view(),name="change-medication-status"),
     path('get-medicines/',MedicineListView.as_view(),name="get-medicines"),
     path('room-details/<int:room_number>/', RoomDetailsView.as_view(), name='room-details'),
     path('auth/', AuthApiView.as_view(), name='auth-api'),
